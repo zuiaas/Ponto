27fa5d1bf873e69ef429f74de4ba60cff5fbc27c
____x_ <josuealexandre067@gmail.com>
2025-07-30 15:31:04 -0400

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import "./styles/styles.css";
+import Home from "./pages/home.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
-    </StrictMode>,
+        <Home/>
+    </StrictMode>
 );
