c037a0bf6fc3676cd35bba103ed7968cfc86b0de
____x_ <josuealexandre067@gmail.com>
2025-07-31 17:07:39 -0400

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,9 +5,11 @@ export default function Home() {
 
     return (
     <>
+
         <div className="w-full h-full bg-gray-950 items-center justify-center">
             <button className="bg-gray-900 flex p-3 rounded-2xl text-white cursor-pointer hover:bg-gray-800">Clique aqui</button>
         </div>
+
     </>
     )
 }
